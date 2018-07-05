a66bed1f57835d84ca508f2d2d78a68958f56689
junjun.li <junjun.li@husor.com>
2018-07-05 22:02:53 +0800

    notice页面

--- Application/Page/Controller/IndexController.class.php
+++ Application/Page/Controller/IndexController.class.php
@@ -15,6 +15,12 @@ class IndexController extends Controller
         echo file_get_contents('https://mp.weixin.qq.com/mp/homepage?__biz=MzAxNTEzNDI2MQ%3D%3D&hid=1&sn=5627a0ef37255833152b015f8f2a5b5a');
     }
 
+    // 公众号官网文件
+    public function  notice() {
+        echo file_get_contents('https://mp.weixin.qq.com/mp/homepage?__biz=MzAxNTEzNDI2MQ%3D%3D&hid=1&sn=5627a0ef37255833152b015f8f2a5b5a');
+
+    }
+
 
 
 }
