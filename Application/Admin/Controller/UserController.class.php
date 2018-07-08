237140392ed05ad6089d1cf167c04d71fa12d413
junjun.li <junjun.li@husor.com>
2018-07-08 21:07:22 +0800

    后台管理系统添加用户删除功能

--- Application/Admin/Controller/UserController.class.php
+++ Application/Admin/Controller/UserController.class.php
@@ -18,16 +18,38 @@ class UserController extends BaseApiController {
      */
     public function all() {
         $this->assign('title', '注册绑定用户');
-        $users = M('User')->select();
+
+
+        // 处理where搜索条件
+        $phone = I('phone');
+        $passport = I('passport');
+        $date = I('date');
+        $where = [];
+        if ($phone) {
+            $where['tel'] = $phone;
+        }
+        if ($passport) {
+            $where['passport'] = $passport;
+        }
+
+        $p = $_GET['p'];
+        $users = D('Api/User')->listByPageWhere($where, $p, 20, 'id desc');
         $users_uid_arr = result_to_array($users, 'id');
         $user_service = new UserService();
         $users_info_list = $user_service->list_more_info_by_uids($users_uid_arr);
 
-        $this->assign('list', $users_info_list)->display();
+        $this->assign('list',$users_info_list);// 赋值数据集
+        $count      = D('Api/User')->countByWhere($where);// 查询满足要求的总记录数
+        $Page       = new Page($count, 20);
+        $page       = $Page->show();// 分页显示输出
+        $this->assign('page_content',$page);// 赋值分页输出
+
+        $this->display();
     }
 
     public function t() {
-        echo  'hello world';
+        $a = S();
+        var_dump($a);
     }
 
 
@@ -71,9 +93,28 @@ class UserController extends BaseApiController {
         if (IS_POST) {
             return $this->success('操作成功');
         }
-        $info = D('UserBack')->where(['id'=>$id])->find();
+        $info = D('Api/UserBack')->where(['id'=>$id])->find();
         $this->assign('info', $info);
         $view = $this->fetch('form');
         $this->ajaxReturn($view);
+    }
+
+
+    /*
+     * 处理删除业务
+     */
+    public function user_del() {
+
+        $id = intval(I('id'));
+        if (!IS_POST || !$id) {
+            return $this->error('你访问的页面不存在');
+        }
+
+        $user_service = new UserService();
+        $user_service->del($id);
+
+        $this->success('操作成功');
+
+
     }
 }
