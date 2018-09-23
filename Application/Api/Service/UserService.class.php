028278c254e860681b821b856f375db21267bec7
李俊君 <hello_lijj@qq.com>
2018-09-23 15:17:43 +0800

    debug4: 修改user接口

--- Application/Api/Service/UserService.class.php
+++ Application/Api/Service/UserService.class.php
@@ -115,7 +115,7 @@ class UserService extends BaseService {
         $passport_arr = result_to_array($user_items, 'passport');
         $user_back_items = D('Api/UserBack')->listByPassports($passport_arr);
         $user_back_items = result_to_map($user_back_items, 'passport');
-        p($user_back_items);die;
+        p($user_back_items);
 
         $weixin_items = D('Api/Weixin')->listByUids($uid_arr);
         $weixin_items = result_to_map($weixin_items, 'uid');
