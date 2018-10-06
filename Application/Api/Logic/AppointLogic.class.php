dff51d27b99ea5bc5c2988c286cd4d6416636782
Junjun Li <junjunli666@gmail.com>
2018-10-06 22:02:30 +0800

    恢复周末不上班的判断

--- Application/Api/Logic/AppointLogic.class.php
+++ Application/Api/Logic/AppointLogic.class.php
@@ -61,12 +61,12 @@ class AppointLogic extends UserBaseLogic {
         }
 
         // 周六周末不允许预约, 暂时先注释掉
-        /*if (date('w', $choose_ts) == 0) {
+        if (date('w', $choose_ts) == 0) {
             return ['status' => 1, 'data' => '周日不上班'];
         }
         if (date('w', $choose_ts) == 6) {
             return ['status' => 1, 'data' => '周六不上班'];
-        }*/
+        }
 
         if ($item == AppointRecordModel::$APPOINT_TYPE_VIA && ($choose_ds - $now_ds) <= 0) {
             return ['status' => 1, 'data' => '签证预约，不能预约今天的日期'];
