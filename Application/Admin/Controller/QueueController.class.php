a62af92119250da537a52600fdc11010fc5ac136
Junjun Li <junjunli666@gmail.com>
2019-01-24 15:22:31 +0800

    debug

--- Application/Admin/Controller/QueueController.class.php
+++ Application/Admin/Controller/QueueController.class.php
@@ -30,14 +30,17 @@ class QueueController {
 
         $mail = new \SaeMail();
 
-        $ret = $mail->quickSend("hello_lijj@qq.com", "这是新浪发给qq的邮件", "李俊君制作", "hello_lijj@sina.com", "hello_lijj", "smtp.sina.com", 25); //指定smtp和端口
+//        $ret = $mail->quickSend( 'to@sina.cn' , '邮件标题' , '邮件内容' , 'smtpaccount@unknown.com' , 'password' , 'smtp.unknown.com' , 25 )
+        $ret = $mail->quickSend("hello_lijj@qq.com", "这是新浪发给qq的邮件", "李俊君制作", "hello_lijj@sina.com", "hello_lijj"); //指定smtp和端口
 
         //发送失败时输出错误码和错误信息
         if ($ret === false) {
             var_dump($mail->errno(), $mail->errmsg());
+        } else {
+            echo '发送成功';
         }
 
-        echo '发送成功';
+
 
     }
 
