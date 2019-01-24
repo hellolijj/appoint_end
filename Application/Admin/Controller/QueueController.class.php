2700e66ca7cc116b1d1bcaed2fc46a046173a0f7
Junjun Li <junjunli666@gmail.com>
2019-01-24 14:50:26 +0800

    add file: 添加队列文件

---
+++ Application/Admin/Controller/QueueController.class.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lijunjun
+ * Date: 2019/1/24
+ * Time: 14:44
+ */
+
+namespace Admin\Controller;
+
+
+class QueueController {
+
+
+    public function download() {
+
+        $t = new SaeTaskQueue("download");
+        $t->addTask("http://" . $_SERVER['HTTP_HOST'] . "/admin.php/Admin/Queue/test"); //添加列队任务1
+        if (!$t->push()) {
+            echo '出错:' . $t->errmsg();
+        } else {
+            echo '执行成功！请查看['  . 'sae_debug.log' . ']文件中的日志';
+        }
+
+    }
+
+    public function test() {
+
+    }
+
+}
